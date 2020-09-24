35937dfda308ea4e30a84af50e624858913f8c97
Iza Skrjanec <skrjanec.iza@gmail.com>
2020-09-24 19:11:24 +0200

    model architecture built

--- models/pytorch/model_components.py
+++ models/pytorch/model_components.py
@@ -1,4 +1,17 @@
-
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchtext
+from torchtext.datasets import Multi30k
+from torchtext.data import Field, BucketIterator, Iterator
+from torchtext.datasets import TranslationDataset
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+import sys
+import random
+import math
+import time
 
 class Encoder(nn.Module):
     def __init__(self,
